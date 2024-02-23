7b9d2c7ef7fc389f9a61b699ee2975bfa1a892e4
ruizleah <leruiz1@s.sfusd.edu>
2024-02-23 15:08:48 -0800

    nb

--- world.gd
+++ world.gd
@@ -1,14 +1,16 @@
 extends Node2D
 
-
+@export var next_level: PackedScene
 
 @onready var level_completed = $CanvasLayer/LevelCompleted
 
 func _ready():
-	RenderingServer.set_default_clear_color(Color.BLACK) 
+	RenderingServer.set_default_clear_color(Color.BLACK)
 	Events.level_completed.connect(show_level_completed)
 	
 
 func show_level_completed():
 	level_completed.show()
+	if not next_level is PackedScene: return
+	get_tree().change_scene_to_packed(next_level)
 	get_tree().paused = true
