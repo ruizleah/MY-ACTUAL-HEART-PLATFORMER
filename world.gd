86bf8312a834c9c76e8902e4a8e4dccdd4fb7dfd
Daphne <121971548+daphnewan@users.noreply.github.com>
2024-02-25 20:38:27 -0800

    up until progress check #11

--- world.gd
+++ world.gd
@@ -5,12 +5,16 @@ extends Node2D
 @onready var level_completed = $CanvasLayer/LevelCompleted
 
 func _ready():
-	RenderingServer.set_default_clear_color(Color.BLACK)
 	Events.level_completed.connect(show_level_completed)
 	
 
 func show_level_completed():
 	level_completed.show()
-	if not next_level is PackedScene: return
-	get_tree().change_scene_to_packed(next_level)
 	get_tree().paused = true
+	await get_tree().create_timer(1.0).timeout
+	if not next_level is PackedScene: return
+	await LevelTransition.fade_to_black()
+	get_tree().paused = false
+	get_tree().change_scene_to_packed(next_level)
+	LevelTransition.fade_from_black()
+
